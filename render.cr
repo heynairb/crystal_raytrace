5eba392bbf7c53d328a9f968140591853f7bd8e9
Brian Yeh <brianyeh@Brians-MacBook-Pro.local>
2018-08-22 00:17:19 -0700

    change color function to color sphere based off of normals

--- render.cr
+++ render.cr
@@ -1,7 +1,26 @@
 require "vector3"
 require "ray"
 
+
+def hit_sphere(center : Vector3, radius : Float64, r : Ray)
+    oc = r.origin() - center
+    a = r.direction().dot(r.direction())
+    b = 2.0 * oc.dot(r.direction())
+    c= oc.dot(oc) - radius**2
+    discriminant = b**2 - 4*a*c
+    if discriminant < 0
+        return -1.0
+    else
+        return (-b - Math.sqrt(discriminant)) / (2.0 * a)
+    end
+end
+
 def color(r : Ray)
+    t = hit_sphere(Vector3.new(0,0,-1), 0.5, r)
+    if t > 0.0
+        n = (r.point_at_parameter(t) - Vector3.new(0,0,-1)).create_unit_vector()
+        return 0.5 * Vector3.new(n.x + 1, n.y + 1, n.z + 1)
+    end
     unit_direction = r.direction().create_unit_vector()
     t = 0.5 * (unit_direction.y() + 1.0)
     (Float64.new(1) - t) * Vector3.new(1.0, 1.0, 1.0)  + t * Vector3.new(0.5, 0.7, 1.0)
